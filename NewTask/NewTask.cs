c639ad6c8e983baeef084783957886697bcd61a6
mIrage_j <windflowerx@gmail.com>
2018-05-15 16:15:32 +0800

    Create the acknowledge receiver(Worker.cs) and sender(NewTask.cs)

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTask
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        var message = GetMessage(args);
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        var propertities = channel.CreateBasicProperties();
+                        propertities.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "task_queue",
+                        basicProperties: propertities,
+                        body: body);
+                    }
+                }
+        }
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.1.150",
+                Port=5672,UserName="test",Password="test"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [x] Received {message}");
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine(" [x] Done");
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    
+                    channel.BasicConsume(queue: "task_queue",
+                        autoAck: false,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
